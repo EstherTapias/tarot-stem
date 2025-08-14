4a5e8b699a3e16bfac2af60f2a38128200467075
EstherTapias <esthertpc93@gmail.com>
2025-08-14 16:09:46 +0200

    Add structure router (#4)

---
+++ src/router/router.tsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from '../components/Layout/Layout';
+import Home from '../pages/Home';
+import CardDetail from '../pages/CardDetail';
+import CardReading from '../pages/CardReading';
+
+/*Define todos las rutas de nuestra aplicación */
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        //Página principal 
+        index: true,
+        element: <Home />,
+      },
+      {
+        //Página de detalle de carta 
+        path: '/carta/:id',
+        element: <CardDetail />,
+      },
+      {
+        //Página de lectura de 3 cartas
+        path: '/lectura',
+        element: <CardReading />,
+      }
+    ]
+  }
+]);
