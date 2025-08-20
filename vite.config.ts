60560a9ef447c0629a090c2cd71ff07a45da7652
EstherTapias <esthertpc93@gmail.com>
2025-08-20 18:07:35 +0200

    first structure and facilities for vitest(#16)

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '127.0.0.1', // fuerza IPv4
-    port: 5173,        // puerto fijo
-    strictPort: true,  // no cambiar de puerto automáticamente
-    open: true         // abrir navegador al arrancar (opcional)
+    host: '127.0.0.1',
+    port: 5173,
+    strictPort: true,
+    open: true,
+  },
+  test: {
+    globals: true,          // permite usar expect() sin importar
+    environment: 'jsdom',   // necesario para componentes React
+    setupFiles: './src/setupTests.ts',
+    exclude: [...configDefaults.exclude], // opcional, excluye node_modules etc
   }
 })
